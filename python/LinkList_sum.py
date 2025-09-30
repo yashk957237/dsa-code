4b56036ab1addcc8db1963105dedb99fc985d536
grow-with-code <growthwithcode@gmail.com>
2025-10-01 00:25:34 +0530

    Added Python DSA code

---
+++ python/LinkList_sum.py
@@ -0,0 +1,14 @@
+l1 = [2,4,3]
+
+l2 = [5,6,4]
+
+k = []
+temp = 0
+for i in range(0,len(l1)):
+    sum1 = l1[i] + l2[i] + temp
+    if sum1//10 > 0:
+        k.append(sum1%10)
+        temp = sum1//10
+    else:
+        k.append(sum1)
+print(k)
