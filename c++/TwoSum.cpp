063d5fbddc453e5419a02c9893011a1716e2e015
emcc2302 <sorifulislamsk1@gmail.com>
2025-10-01 23:43:29 +0530

    Adding TwoSum file

---
+++ c++/TwoSum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(const vector<int>& nums, int target) {
+    unordered_map<int, int> numIndex; 
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (numIndex.find(complement) != numIndex.end()) {
+            return {numIndex[complement], i};
+        }
+        numIndex[nums[i]] = i;
+    }
+    return {}; 
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No two sum solution found." << endl;
+    }
+
+    return 0;
+}
