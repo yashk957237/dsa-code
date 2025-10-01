cf5560cd1d4785581bc145bca6fc58a1d2eec23a
Ankit Kumar Yadav <67411716+AnkitKrYadav@users.noreply.github.com>
2025-10-01 10:58:00 +0000

    added factorial soln.

--- python/factorial.py
+++ python/factorial.py
@@ -1,7 +1,9 @@
 n=int(input("Enter number to find factorial of: "))
+
 def factorial(n):
     if n==0:
         return 1
     else:
         return n*factorial(n-1)
-print("Factorial of",n,"is",factorial(n))
+    
+print("Factorial of",n,"is: ",factorial(n))
