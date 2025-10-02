de2d94be5b99f6144ad6bab1d412846a4ef3885b
Singhai17 <divyanshsinghai1710@gmail.com>
2025-10-02 21:56:22 +0530

    Implemented Merge Sort in Python

---
+++ python/MergeSort.py
@@ -0,0 +1,34 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+def merge(left, right):
+    res = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            res.append(left[i])
+            i += 1
+        else:
+            res.append(right[j])
+            j += 1
+
+    res.extend(left[i:])
+    res.extend(right[j:])
+    return res
+
+# Example
+data = [12, 11, 13, 5, 6, 7]
+sorted_data = merge_sort(data)
+print(f"\nOriginal: {data}")
+print(f"Sorted: {sorted_data}")
