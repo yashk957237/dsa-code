1953e5aeae303ad32c6f16fba0e74b5a1049fc37
ChetanFTW <devchetan42@gmail.com>
2025-10-02 14:56:02 +0100

    Algorithm for recursion and Backtracking

---
+++ python/sudoku_solver.py
@@ -0,0 +1,47 @@
+def is_valid(board, row, col, num):
+    for x in range(9):
+        if board[row][x] == num or board[x][col] == num:
+            return False
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + startRow][j + startCol] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+def print_board(board):
+    for row in board:
+        print(row)
+
+# Example 9x9 board with 0 as empty cells
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(board):
+    print("Sudoku solved:")
+    print_board(board)
+else:
+    print("No solution exists")
