24c52f392659dba2c88d3e488d3dcafe77210ad1
RoshanKumar9421 <yadavroshankumar8271@gmail.com>
2025-10-02 18:51:12 +0530

    two sum

--- c++/Twosum.cpp
+++ c++/Twosum.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mp;  // value -> index
+        unordered_map<int,int> mp;  
         for(int i=0; i<nums.size(); i++){
             int complement = target - nums[i];
             if(mp.find(complement) != mp.end()){
