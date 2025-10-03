f3b52adb290e81d75bb0ce3a0bd27fa0d2bc652a
Smarak <smarakdas16@gmail.com>
2025-10-03 22:53:39 +0530

    Create Stack-valid_parantheses.py
    Check if parentheses are valid.

---
+++ Stack-valid_parantheses.py
@@ -0,0 +1,15 @@
+# Problem: Check if parentheses are valid.
+
+def isValid(s: str) -> bool:
+    stack = []
+    mapping = {")":"(", "}":"{", "]":"["}
+    for char in s:
+        if char in mapping:
+            top = stack.pop() if stack else "#"
+            if mapping[char] != top:
+                return False
+        else:
+            stack.append(char)
+    return not stack
+
+print(isValid("()[]{}"))  # True
