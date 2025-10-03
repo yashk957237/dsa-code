28c769ec4e2f6b072a62c6dd9da702b6baa350b3
ghostmanish <mgoshwami19@gmail.com>
2025-10-03 18:31:16 +0530

    merge

--- python/linear_search.py
+++ python/linear_search.py
@@ -1,5 +1,5 @@
 def linear_search(arr, target):
-<<<<<<< HEAD
+
     for index, value in enumerate(arr):  
         if value == target:
             return index  
@@ -8,7 +8,7 @@ def linear_search(arr, target):
 # Example
 numbers = [10, 20, 30, 40, 50]
 target = 30
-=======
+
     for i in range(len(arr)):
         if arr[i] == target:
             return i
@@ -19,4 +19,4 @@ arr = [4, 2, 7, 1, 9]
 target = 7
 result = linear_search(arr, target)
 print(f"Element found at index: {result}" if result != -1 else "Element not found")
->>>>>>> main
+
