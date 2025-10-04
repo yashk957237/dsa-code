7b6f828a2428e80bda885cd36bdfd8f43db74199
imkmaurya <kavyamaurya47@gmail.com>
2025-10-04 10:23:51 +0530

    Added c++ DSA code

--- c++/Binary Trees/top_view_tree.cpp
+++ c++/Binary Trees/top_view_tree.cpp
@@ -1,111 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define f(i, a, b) for (int i = a; i < b; i++)
-#define all(x) x.begin(), x.end()
+#define vint vector<int>
 #define vprint(v)            \
     for (auto &elem : v)     \
         cout << elem << " "; \
     cout << endl;
-#define mprint(m)                                       \
-    for (auto it : m)                                   \
-        cout << it.first << " : " << it.second << endl; \
-    cout << endl;
-#define vint vector<int>
-#define vstring vector<string>
-#define vmat vector<vector<int>>
 #define FAST_IO                       \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define mii map<int, int>
-#define mll map<ll, ll>
 
-const int INF = 1e9;
-const ll MOD = 1e9 + 7;
-
-struct TreeNode
-{
+struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-vector<int> topView(TreeNode *root)
-{
+// BFS traversal collecting nodes by (x,y) without structured bindings
+map<int, map<int, multiset<int>>> bfsCollect(TreeNode* root) {
     map<int, map<int, multiset<int>>> nodes;
-    queue<pair<TreeNode *, pair<int, int>>> todo;
-    todo.push({root, {0, 0}});
-    while (!todo.empty())
-    {
-        auto p = todo.front();
-        todo.pop();
-        TreeNode *node = p.first;
+    if (!root) return nodes;
+
+    queue<pair<TreeNode*, pair<int,int>>> q;
+    q.push(make_pair(root, make_pair(0, 0)));
+
+    while (!q.empty()) {
+        pair<TreeNode*, pair<int,int>> p = q.front();
+        q.pop();
+
+        TreeNode* node = p.first;
         int x = p.second.first;
         int y = p.second.second;
-        nodes[x][y].insert(node->val);
-        if (node->left)
-        {
-            todo.push({node->left, {x - 1, y + 1}});
-        }
-        if (node->right)
-        {
-            todo.push({node->right, {x + 1, y + 1}});
-        }
-    }
-    vector<int> ans;
-    for (auto p : nodes)
-    {
-        auto it = p.second.begin();
-        int firstVal = *it->second.begin();
-        ans.push_back(firstVal);
-    }
 
+        nodes[x][y].insert(node->val);
+
+        if (node->left)
+            q.push(make_pair(node->left, make_pair(x - 1, y + 1)));
+        if (node->right)
+            q.push(make_pair(node->right, make_pair(x + 1, y + 1)));
+    }
+    return nodes;
+}
+
+vint topView(TreeNode* root) {
+    vint ans;
+    auto nodes = bfsCollect(root);
+
+    // For each horizontal distance, pick the topmost node (smallest y)
+    for (auto it = nodes.begin(); it != nodes.end(); ++it) {
+        auto &levelMap = it->second;
+        auto levelIt = levelMap.begin(); // smallest y
+        ans.push_back(*levelIt->second.begin());
+    }
     return ans;
 }
 
-vector<int> bottomView(TreeNode *root)
-{
-    map<int, map<int, multiset<int>>> nodes;
-    queue<pair<TreeNode *, pair<int, int>>> todo;
-    todo.push({root, {0, 0}});
-    while (!todo.empty())
-    {
-        auto p = todo.front();
-        todo.pop();
-        TreeNode *node = p.first;
-        int x = p.second.first;
-        int y = p.second.second;
-        nodes[x][y].insert(node->val);
-        if (node->left)
-        {
-            todo.push({node->left, {x - 1, y + 1}});
-        }
-        if (node->right)
-        {
-            todo.push({node->right, {x + 1, y + 1}});
-        }
-    }
-    vector<int> ans;
-    for (auto p : nodes)
-    {
-        auto it = prev(p.second.end());
-        int firstVal = *prev(it->second.end());
-        ans.push_back(firstVal);
-    }
+vint bottomView(TreeNode* root) {
+    vint ans;
+    auto nodes = bfsCollect(root);
 
+    // For each horizontal distance, pick the bottommost node (largest y)
+    for (auto it = nodes.begin(); it != nodes.end(); ++it) {
+        auto &levelMap = it->second;
+        auto levelIt = --levelMap.end(); // largest y
+        ans.push_back(*--(levelIt->second.end()));
+    }
     return ans;
 }
 
-int main()
-{
+int main() {
     FAST_IO;
+
     TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->right = new TreeNode(7);
@@ -118,8 +85,13 @@ int main()
     root->right->right->left->left = new TreeNode(10);
     root->right->right->left->right = new TreeNode(11);
 
-    vint ans = bottomView(root);
-    vprint(ans);
+    vint ansBottom = bottomView(root);
+    cout << "Bottom View: ";
+    vprint(ansBottom);
+
+    vint ansTop = topView(root);
+    cout << "Top View: ";
+    vprint(ansTop);
+
     return 0;
 }
-// by ad73prem
