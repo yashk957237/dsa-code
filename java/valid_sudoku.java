6854fb45d8908a21c50ebe20eb95395f6592eb5f
vaibhav kottari <kottarivaibhav@gmail.com>
2025-10-09 10:17:56 +0530

    Added a valid_sudoku solution in java . Hacktoberfest solution

---
+++ java/valid_sudoku.java
@@ -0,0 +1,45 @@
+public class valid_sudoku {
+
+    public static void main(String[] args) {
+        // Sample Sudoku board
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        Solution solution = new Solution();
+        boolean isValid = solution.isValidSudoku(board);
+        System.out.println("Is the Sudoku board valid? " + isValid);
+    }
+}
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                        return false;
+                    }
+
+                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+                }
+            }
+        }
+        return true;
+    }
+}
