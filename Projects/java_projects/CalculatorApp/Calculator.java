c5932cbae06c2f832b9de39bace688c0c38dc934
chetanbhasneydev <luckybhasney02@gmail.com>
2025-10-13 15:48:43 +0530

    Add Java Calculator project under Projects/java_projects

---
+++ Projects/java_projects/CalculatorApp/Calculator.java
@@ -0,0 +1,23 @@
+package CalculatorApp;
+
+public class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero!");
+            return Double.NaN;
+        }
+        return a / b;
+    }
+}
