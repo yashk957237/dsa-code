f9d9d12936212d28c6c308684b9a262439de0b70
chetanbhasneydev <luckybhasney02@gmail.com>
2025-10-13 15:26:20 +0530

    Add Fibonacci program in Java folder

---
+++ java/Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10, first = 0, second = 1;
+        System.out.print("Fibonacci Series: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
