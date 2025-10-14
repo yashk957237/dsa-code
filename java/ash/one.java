c4e9d56464213fdfe340bd00f72ff8dceaccc8a6
Ashish Ingle <ashishingle369@gmail.com>
2025-10-14 14:32:46 +0530

    one.java committed

---
+++ java/ash/one.java
@@ -0,0 +1,11 @@
+public class AddStrings {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+
+        // Adding (concatenating) two strings
+        String result = str1 + " " + str2;
+
+        System.out.println("Result: " + result);
+    }
+}
